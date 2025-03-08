5d29d206ff8feeef3b2830429f0dcc6f5b66498e
LancemDev <munyaolance1@gmail.com>
2025-03-08 12:58:24 +0300

    Update button styles and change index route method to GET

--- api/index.py
+++ api/index.py
@@ -2,6 +2,6 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET'])
 def index():
     return jsonify({'message': 'Hello, World!'})
